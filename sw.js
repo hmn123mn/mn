0dc0c42be99515068e20a49bf953438724a0ee90
wuhaomeng <wuhaomeng@chngalaxy.com>
2017-11-24 14:26:22 +0800

    []

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
+importScripts('serviceworker-cache-polyfill.js');
+
 self.onfetch = (e)=> {
 	e.respondWith(new Response('hello sw'))
 }
+
+
+var CACHE_NAME = 'my-site-cache-v1';
+// The files we want to cache
+var urlsToCache = [
+  '/',
+  '/style.css'
+];
+
+// Set the callback for the install step
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+	    caches.open(CACHE_NAME)
+	      .then(function(cache) {
+	        console.log('Opened cache');
+	        return cache.addAll(urlsToCache);
+	      })
+    );
+});
