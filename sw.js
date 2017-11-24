c227b0316ef9f5cbfbabdbf9a64f441d20038f04
wuhaomeng <wuhaomeng@chngalaxy.com>
2017-11-24 18:19:05 +0800

    [*]

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // }
 
 
-var CACHE_NAME = ['my-site-cache-v34'];
+var CACHE_NAME = ['my-site-cache-v36'];
 // The files we want to cache
 var urlsToCache = [
   './index.html',
@@ -13,6 +13,7 @@ var urlsToCache = [
 // Set the callback for the install step
 self.addEventListener('install', function(event) {
     // Perform install steps
+    self.skipWaiting();
     console.log('install',event);
     event.waitUntil(
 	    caches.open(CACHE_NAME[0])
@@ -45,43 +46,43 @@ self.addEventListener('activate', function(event) {
   return self.clients.claim();
 });
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         // Cache hit - return response
-//         if (response) {
-//           return response;
-//         }
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
 
-//         // IMPORTANT: Clone the request. A request is a stream and
-//         // can only be consumed once. Since we are consuming this
-//         // once by cache and once by the browser for fetch, we need
-//         // to clone the response
-//         var fetchRequest = event.request.clone();
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response
+        var fetchRequest = event.request.clone();
 
-//         return fetch(fetchRequest).then(
-//           function(response) {
-//             // Check if we received a valid response
-//             if(!response || response.status !== 200 || response.type !== 'basic') {
-//               return response;
-//             }
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
 
-//             // IMPORTANT: Clone the response. A response is a stream
-//             // and because we want the browser to consume the response
-//             // as well as the cache consuming the response, we need
-//             // to clone it so we have 2 stream.
-//             var responseToCache = response.clone();
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have 2 stream.
+            var responseToCache = response.clone();
 
-//             caches.open(CACHE_NAME)
-//               .then(function(cache) {
-//                 cache.put(event.request, responseToCache);
-//               });
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
 
-//             return response;
-//           }
-//         );
-//       })
-//     );
-// });
+            return response;
+          }
+        );
+      })
+    );
+});
 
