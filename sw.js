182c159535e83f3d0e30d0c6a09c8b7f588bff9b
wuhaomeng <wuhaomeng@chngalaxy.com>
2017-12-20 16:52:59 +0800

    76

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // }
 
 
-var CACHE_NAME = 'my-site-cache-v77';
+var CACHE_NAME = 'my-site-cache-v78';
 // The files we want to cache
 var urlsToCache = [
   './index.html',
@@ -27,28 +27,19 @@ self.addEventListener('install', function(event) {
 
 
 self.addEventListener('activate', event => event.waitUntil(
-    // Promise.all([
-    //     // 更新客户端
-    //     clients.claim(),
-    //     // 清理旧版本
-    //     caches.keys().then(cacheList => Promise.all(
-    //         cacheList.map(cacheName => {
-    //             if (cacheName !== CACHE_NAME) {
-    //                 caches.delete(cacheName);
-    //             }
-    //         })
-    //     ))
-    // ])
+    Promise.all([
+        // 更新客户端
+        clients.claim(),
+        // 清理旧版本
+        caches.keys().then(cacheList => Promise.all(
+            cacheList.map(cacheName => {
+                if (cacheName !== CACHE_NAME) {
+                    caches.delete(cacheName);
+                }
+            })
+        ))
+    ])
 
-    event.waitUntil(
-       caches.keys().then(keyList => {
-           return Promise.all(keyList.map(key => {
-               if (CACHE_NAME.indexOf(key) === -1) {
-                   return caches.delete(key);
-               }
-           }));
-       })
-    );
 ));
 
 
