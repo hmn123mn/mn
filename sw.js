9b7357ae9743d956cab74dc7c12d60319a56e5d3
wuhaomeng <wuhaomeng@chngalaxy.com>
2017-12-20 16:32:18 +0800

    []

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // }
 
 
-var CACHE_NAME = 'my-site-cache-v72';
+var CACHE_NAME = 'my-site-cache-v73';
 // The files we want to cache
 var urlsToCache = [
   './index.html',
@@ -13,15 +13,16 @@ var urlsToCache = [
 // Set the callback for the install step
 self.addEventListener('install', function(event) {
     // Perform install steps
-    self.skipWaiting();
-    console.log('install',event);
-    // event.waitUntil(
-	  //   caches.open(CACHE_NAME)
-	  //     .then(function(cache) {
-	  //       console.log('Opened cache');
-	  //       return cache.addAll(urlsToCache);
-	  //     })
-    // );
+    // self.skipWaiting();
+    // console.log('install',event);
+    event.waitUntil(
+	    caches.open(CACHE_NAME)
+	      .then(function(cache) {
+	        console.log('Opened cache');
+	        return cache.addAll(urlsToCache);
+	      })
+        .then(() => self.skipWaiting())
+    );
 });
 
 
