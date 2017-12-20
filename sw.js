d898ad65d8fa868175d57be903f1e4c33d07239b
wuhaomeng <wuhaomeng@chngalaxy.com>
2017-12-20 15:58:45 +0800

    [+]manifest.json

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // }
 
 
-var CACHE_NAME = 'my-site-cache-v60';
+var CACHE_NAME = 'my-site-cache-v66';
 // The files we want to cache
 var urlsToCache = [
   './index.html',
@@ -80,4 +80,3 @@ self.addEventListener('fetch', function(event) {
       })
     );
 });
-
